cf844a1dba86fb3e9978961267ef112368325f03
Rich Garbutt <rich.garbutt@gmail.com>
2014-04-10 14:47:50 +0100

    Added middleman skeleton; Added bootstrap-sass, slim;  modified configs; added base source file; added template (slim) and index.html

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'middleman', '~> 3.2.2'
+
+# Live-reloading plugin
+gem 'middleman-livereload', '~> 3.1.1'
+# Deploy plugin
+gem 'middleman-deploy', '~> 0.1.4'
+
+# For faster file watcher updates on Windows:
+#gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
+
+gem 'slim', '~> 2.0.2'
+
+gem 'bootstrap-sass', '~> 3.1.0'
---
+++ config.rb
@@ -0,0 +1,100 @@
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (http://middlemanapp.com/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+activate :livereload
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'css'
+
+set :js_dir, 'js'
+
+set :images_dir, 'img'
+
+# Configure Slim
+Slim::Engine.set_default_options pretty: true, sort_attrs: false
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  compass_config do |config|
+    config.output_style = :expanded
+    config.line_comments = false
+  end
+
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+  set :relative_links, true
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+
+  # Minify HTML on build
+  # Slim::Engine.set_default_options pretty: false
+end
+
+# middleman-deploy configuration
+activate :deploy do |deploy|
+  # Automatically run `middleman build` during `middleman deploy`
+  # deploy.build_before = true
+
+  # rsync, ftp, sftp, or git
+  deploy.method = :git
+
+  # remote name or git url, default: origin
+  # deploy.remote   = 'custom-remote'
+
+  # default: gh-pages
+  # deploy.branch   = 'master'
+
+  # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.strategy = :submodule
+end
